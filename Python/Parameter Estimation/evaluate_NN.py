1c147d0890dbe6be48087e5dc16fe63b306750c5
Emil <emelf@usn.no>
2020-10-22 15:34:23 +0200

    Added a new method for param est! Dosent work YET!

--- Python/Parameter Estimation/evaluate_NN.py
+++ Python/Parameter Estimation/evaluate_NN.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd 
 
 class TestNN: 
-    def __init__(self, NN_model, dynamical_model, data_model): 
+    def __init__(self, NN_model, dynamical_model, data_model, n_y=6, y_include=[0,1,2,3,4,5]): 
         self.NN_model = NN_model 
         self.dyn_model = dynamical_model 
         self.data_model = data_model 
+        self.n_y = n_y
+        self.y_include = np.array(y_include)
 
     def find_theta_one(self): 
         u = self.dyn_model.find_input_vals() 
@@ -15,6 +17,14 @@ class TestNN:
         X_NN = np.append(x_sol, dxdt_sol, axis=1)
         X_NN = np.append(X_NN, u, axis=1)
         y_NN = np.array([np.ones(len(X_NN))*th for th in theta_rand]).T 
+
+        if len(self.y_include) < self.n_y:
+            n = np.linspace(0, self.n_y-1, self.n_y, dtype=np.int8)
+            n_x = np.delete(n, self.y_include)
+            X_NN = np.append(X_NN.T, y_NN.T[n_x], axis=0).T
+            y_NN = y_NN[:,self.y_include]
+
+
         X_NN = self.data_model.X_scaler.transform(X_NN)
         y_pred = self.NN_model.predict(X_NN)
         y_pred = self.data_model.y_scaler.inverse_transform(y_pred)
